5c87268b470de32d9e36b7521e4ad283af775f52
Raghav <rgoel8690@gmail.com>
2020-09-12 23:22:13 +0530

    m

--- controller/reviewController.js
+++ controller/reviewController.js
@@ -3,9 +3,9 @@ const factory = require('../utility/factory');
 
 module.exports.getReview = factory.getElement(reviewModel);
 module.exports.getAllReviews = factory.getAllElement(reviewModel);
-module.exports.updateReview = factory.updateElement(reviewModel)
-module.exports.deleteReview = factory.deleteElement(reviewModel)
-module.exports.createReview = factory.createElement(reviewModel)
+module.exports.updateReview = factory.updateElement(reviewModel);
+module.exports.deleteReview = factory.deleteElement(reviewModel);
+module.exports.createReview = factory.createElement(reviewModel);
 
 // async function createReview(req, res) {
 //   try {
